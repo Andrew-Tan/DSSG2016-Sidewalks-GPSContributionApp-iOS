e550cd1e941e919697370e54b24321b12f1137df
Kaicheng Tan <tan95@cs.washington.edu>
2016-06-29 09:53:22 -0700

    Initial Commit

---
+++ GPS MarkerTests/GPS_MarkerTests.swift
@@ -0,0 +1,36 @@
+//
+//  GPS_MarkerTests.swift
+//  GPS MarkerTests
+//
+//  Created by Andrew Tan on 6/29/16.
+//  Copyright © 2016 Taskar Center for Accessible Technology. All rights reserved.
+//
+
+import XCTest
+@testable import GPS_Marker
+
+class GPS_MarkerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
